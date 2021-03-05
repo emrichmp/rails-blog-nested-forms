c84d9ec59bf6a379b3674ac28528688a290c11ac
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-04 19:06:13 -0500

    commit tree 03b15e1aa474c0fb64d70bff57e5dc4633f23cda

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,15 +1,15 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe Tag do
-  describe ":name" do
-    it "should be unique" do
-      expect {
-        Tag.create(name: 'tag')
-      }.to change(Tag, :count).by(1)
+# describe Tag do
+#   describe ":name" do
+#     it "should be unique" do
+#       expect {
+#         Tag.create(name: 'tag')
+#       }.to change(Tag, :count).by(1)
 
-      expect{
-        Tag.create!(name: 'tag')
-      }.to raise_error(ActiveRecord::RecordInvalid)
-    end
-  end
-end
+#       expect{
+#         Tag.create!(name: 'tag')
+#       }.to raise_error(ActiveRecord::RecordInvalid)
+#     end
+#   end
+# end
